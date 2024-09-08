50e7865ebcde083e28ea619f2e11dcf261eb2911
KonurYigit <konuralpyigit@gmail.com>
2024-09-07 22:26:44 -0500

    ModelSelect and ControlSelect files have been modified to open model before the update.

--- scripts/ControlSelect.m
+++ scripts/ControlSelect.m
@@ -2,14 +2,24 @@ function ControlSelect
 list = {"LQR Model State Feedback","LQR True State Feedback","LQR With Integrator"};   
 [indx,tf] = listdlg('PromptString',{'Select Control Variant'},...
     'SelectionMode','single','ListString',list);
+isModelOpen = bdIsLoaded('top_model');
 switch indx
     case 1
       assignin("base","ControlVariant",1);
+      if ~isModelOpen
+          open_system("top_model");
+      end
       set_param('top_model','SimulationCommand','Update');
     case 2
+      if ~isModelOpen
+          open_system("top_model");
+      end
       assignin("base","ControlVariant",2);
       set_param('top_model','SimulationCommand','Update');
     case 3
+      if ~isModelOpen
+          open_system("top_model");
+      end
       assignin("base","ControlVariant",3);
       set_param('top_model','SimulationCommand','Update');
 
--- scripts/ModelSelect.m
+++ scripts/ModelSelect.m
@@ -2,15 +2,25 @@ function ModelSelect
 list = {"NonlinearDynamicsModel","NonlinearKinematicsModel", "LinearDynamicsModel"};   
 [indx,tf] = listdlg('PromptString',{'Select a Vehicle Model.'},...
     'SelectionMode','single','ListString',list);
+isModelOpen = bdIsLoaded('top_model');
+
 switch indx
     case 1
       assignin("base","ModelType",1);
+      if ~isModelOpen
+          open_system("top_model");
+      end
       set_param('top_model','SimulationCommand','Update');
-      
     case 2
       assignin("base","ModelType",2);
+      if ~isModelOpen
+          open_system("top_model");
+      end
       set_param('top_model','SimulationCommand','Update');
     case 3
+      if ~isopen
+          open_system("top_model");
+      end
       assignin("base","ModelType",3);
 
 end
